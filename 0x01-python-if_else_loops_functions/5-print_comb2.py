11760741103e3d447a5373723093aab7ca5bff08
HanRachid <handaouirachid@gmail.com>
2023-06-06 15:47:08 +0100

    add task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 for i in range(100):
-
     if (i == 99):
         print(99)
         continue
-    if (i < 10):
-        print("0{}, ".format(i), end='')
     else:
-        print("{}, ".format(i), end='')
+        print("{:02d} ,".format(i), end='')
