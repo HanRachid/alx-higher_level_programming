d42123640f1efea671c37dc0f78d897ddcbf6465
HanRachid <handaouirachid@gmail.com>
2023-06-08 19:56:54 +0100

    start 0X02

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from sys import argv
-print("{} arguments:".format(len(argv)-1))
-for i in range(len(argv)-1):
-    print("{}: {}".format(i+1, argv[i+1]))
+if __name__ == "__main__":
+    print("{} arguments:".format(len(argv)-1))
+    for i in range(len(argv)-1):
+        print("{}: {}".format(i+1, argv[i+1]))
