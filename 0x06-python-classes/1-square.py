873864f0980f85b39660bea68395cc7928436fe0
HanRachid <handaouirachid@gmail.com>
2023-06-28 00:13:19 +0100

    add task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+
+
+""" Module providing an empty definition of a class 'Square'
+"""
+
+
 class Square:
+    """ square size
+    """
+
     def __init__(self, size):
         self.__size = size
