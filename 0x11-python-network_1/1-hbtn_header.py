71fbcf0c8adb474184a2f92a35b252e2b88e0110
HanRachid <handaouirachid@gmail.com>
2023-12-04 21:32:50 +0100

    add task 0 and 1

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""sends a request to the URL and displays the request id in the header of the response
+"""
+import urllib.request
+import sys
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.getheader("X-Request-Id"))
